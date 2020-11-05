b854c40040980bc92cc51440f68c39f45e398bb3
xiaomage2000 <d1540076394@gmail.com>
2020-11-05 17:10:36 +0800

    更新版本 添加服务器

--- config.js
+++ config.js
@@ -12,8 +12,10 @@ window.Config = {
   ApiKeys: [
     'm783164825-f1500f2b44408fa1b0685a1f',//main
     'm783164881-bef5a6635e49d87fa104179a',//blog
-    'm783164885-b94c3b94fcc051f5af51b9e8',//lab
-//    'm783908908-46f7ce6a6285c669eff442a9',//v2ray源
+    'm783164885-b94c3b94fcc051f5af51b9e8',//bbx
+    'm786305202-d55555945ffaf60a1c800a18',//akko
+    'm786305205-a5229a608c92b6f29d4e4b2d',//rn
+
   ],
 
   // 是否显示监测站点的链接
@@ -22,7 +24,7 @@ window.Config = {
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 45,
+  CountDays: 75,
 
   // 导航栏菜单
   Navi: [
